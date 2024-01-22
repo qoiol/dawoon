0bb2908ed47fe7d60800bf83759dafd4b2903d19
USER <hjjj1324@gmail.com>
2024-01-23 01:27:50 +0900

    회원가입 기능 수정
    Signed-off-by: USER <hjjj1324@gmail.com>

--- project/src/main/java/spring/project/Controller/UserController.java
+++ project/src/main/java/spring/project/Controller/UserController.java
@@ -18,29 +18,36 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/login/form")
+    @GetMapping("/user/login/form") //로그인 화면으로 이동
     public String loginForm(){
         return "/user/login";
     }
-    @PostMapping("/user/login")
+    @PostMapping("/user/login") //로그인
     public String login(LoginForm loginForm, Model model){
         //로그인 기능 구현
         return "redirect:/";
     }
-    @GetMapping("/user/logout")
+    @GetMapping("/user/logout") //로그아웃 -> 메인페이지로
     public String logout(Model model){
         model.addAttribute("userId", null);
         return "redirect:/";
     }
-    @GetMapping("/user/create/form")
+    @GetMapping("/user/create/form") //회원가입 화면으로 이동
     public String createForm(){
         return "/user/create";
     }
-    @PostMapping("/user/create")
-    public String create(CreateForm createForm){
+    @PostMapping("/user/create") //회원가입
+    public String create(CreateForm createForm, Model model){
         System.out.println("create");
 
-        userService.validateDuplicateUser(createForm.getId());
+        try{
+            userService.validateDuplicateUser(createForm.getId());
+        }catch (Exception e){
+            model.addAttribute("registerFailed", true);
+            model.addAttribute("exception", e.getMessage());
+            return "/user/create";
+        }
+
 
         User user = new User();
         user.setId(createForm.getId());
@@ -48,6 +55,8 @@ public class UserController {
         user.setUserType(createForm.getUserType());
         user.setPassword(createForm.getPassword());
         user.setName(createForm.getName());
+
+        System.out.println("user.getId() = " + user.getId());
 
         userService.join(user);
 

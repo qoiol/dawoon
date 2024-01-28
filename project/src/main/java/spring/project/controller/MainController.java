803753d96c34d2f9cf98441ccf6e46ddc2d9bfaa
USER <hjjj1324@gmail.com>
2024-01-28 18:26:22 +0900

    review
    Signed-off-by: USER <hjjj1324@gmail.com>

--- project/src/main/java/spring/project/controller/MainController.java
+++ project/src/main/java/spring/project/controller/MainController.java
@@ -3,8 +3,8 @@ package spring.project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import spring.project.domain.Exercise;
 import spring.project.domain.User;
+import spring.project.domain.Workout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +19,7 @@ public class MainController {
     @GetMapping("/main") //메인페이지
     public String mainPage(Model model){
 
-        List<Exercise> ex = new ArrayList<>();
+        List<Workout> ex = new ArrayList<>();
 
         //top3 인기 운동 조회
 

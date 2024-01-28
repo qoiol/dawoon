fe74450c5058dc5fdd1e27c67a3ed6e1ab8b9bc9
USER <hjjj1324@gmail.com>
2024-01-28 18:06:35 +0900

    review
    Signed-off-by: USER <hjjj1324@gmail.com>

--- project/src/main/java/spring/project/service/ReviewService.java
+++ project/src/main/java/spring/project/service/ReviewService.java
@@ -1,4 +1,30 @@
 package spring.project.service;
 
+import org.springframework.stereotype.Service;
+import spring.project.domain.Review;
+import spring.project.repository.ReviewRepository;
+
+import java.util.List;
+
+@Service
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public long createReview(Review review){
+        review.setLikeCount(0);
+        reviewRepository.save(review);
+        return review.getId();
+    }
+
+    public List<Review> findReviews(){
+        return reviewRepository.findAll();
+    }
+
+    public void delete(long id){
+        reviewRepository.deleteById(id);
+    }
 }

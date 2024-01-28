8598f1280a06fbeea92dd2efb59f0d9bc2de70a1
qoiol <80204416+qoiol@users.noreply.github.com>
2024-01-28 18:29:48 +0900

    Update WorkoutController.java

--- project/src/main/java/spring/project/Controller/WorkoutController.java
+++ project/src/main/java/spring/project/Controller/WorkoutController.java
@@ -1,4 +1,4 @@
-package spring.project.Controller;
+package spring.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;

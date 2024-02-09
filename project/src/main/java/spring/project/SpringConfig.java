5adae7b887922a516426fa627aef6779b847e25c
USER <hjjj1324@gmail.com>
2024-02-09 21:26:38 +0900

    config 수정 reviewservice 수정
    Signed-off-by: USER <hjjj1324@gmail.com>

--- project/src/main/java/spring/project/SpringConfig.java
+++ project/src/main/java/spring/project/SpringConfig.java
@@ -1,5 +1,7 @@
 package spring.project;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,33 +9,43 @@ import spring.project.repository.*;
 import spring.project.service.ReportService;
 import spring.project.service.ReviewService;
 import spring.project.service.UserService;
+import spring.project.service.WorkoutService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final UserRepository userRepository;
+//    private final UserRepository userRepository;
     private final ReviewRepository reviewRepository;
     private final ReportRepository reportRepository;
+    private final LikeyRepository likeyRepository;
+//    private final WorkoutRepository workoutRepository;
 
-    @Autowired
-    public SpringConfig(UserRepository userRepository, ReviewRepository reviewRepository, ReportRepository reportRepository) {
-        this.userRepository = userRepository;
+    private final EntityManager em;
+    private final DataSource dataSource;
+
+    public SpringConfig(ReviewRepository reviewRepository, ReportRepository reportRepository, LikeyRepository likeyRepository, EntityManager em, DataSource dataSource) {
         this.reviewRepository = reviewRepository;
         this.reportRepository = reportRepository;
+        this.likeyRepository = likeyRepository;
+        this.em = em;
+        this.dataSource = dataSource;
     }
 
     @Bean
-    public UserService userService(){
-        return new UserService(userRepository);
+    public UserService userService() {
+        return new UserService(userRepository());
     }
 
     @Bean
-    public UserRepository userRepository(){
-        return new MemoryUserRepository();
+    public UserRepository userRepository() {
+//        return new MemoryUserRepository();
+        return new JpaUserRepository(em);
     }
 
     @Bean
     public ReviewService reviewService() {
-        return new ReviewService(reviewRepository);
+        return new ReviewService(reviewRepository, likeyRepository);
     }
 
     @Bean
@@ -42,12 +54,18 @@ public class SpringConfig {
     }
 
     @Bean
+    public LikeyRepository likeyRepository() {
+        return new MemoryLikeyRepository();
+    }
+
+    @Bean
     public ReportService reportService() {
         return new ReportService(reportRepository);
     }
 
     @Bean
-    public ReportRepository reportRepository(){
+    public ReportRepository reportRepository() {
         return new MemoryReportRepository();
     }
+
 }
--- project/src/main/java/spring/project/service/ReviewService.java
+++ project/src/main/java/spring/project/service/ReviewService.java
@@ -2,6 +2,7 @@ package spring.project.service;
 
 import org.springframework.stereotype.Service;
 import spring.project.domain.Review;
+import spring.project.repository.LikeyRepository;
 import spring.project.repository.ReviewRepository;
 
 import java.util.List;
@@ -9,10 +10,13 @@ import java.util.List;
 @Service
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+    private final LikeyRepository likeyRepository;
 
-    public ReviewService(ReviewRepository reviewRepository) {
+    public ReviewService(ReviewRepository reviewRepository, LikeyRepository likeyRepository) {
         this.reviewRepository = reviewRepository;
+        this.likeyRepository = likeyRepository;
     }
+
 
     public long createReview(Review review){
         review.setLikeCount(0);

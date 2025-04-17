d38431b4a378546729cbb368517fe1e6da2bd132
qoiol <hjjj1324@gmail.com>
2025-04-17 22:51:12 +0900

    user +login 토큰 인증하도록 수정 +비밀번호 암호화 추가 +login 후 이전 페이지로 이동 추가
    Signed-off-by: qoiol <hjjj1324@gmail.com>

--- project/src/main/java/spring/project/controller/UserController.java
+++ project/src/main/java/spring/project/controller/UserController.java
@@ -1,83 +1,123 @@
 package spring.project.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import spring.project.domain.User;
-import spring.project.dto.CreateForm;
-import spring.project.dto.LoginForm;
+import spring.project.dto.UserJoinRequest;
+import spring.project.dto.LoginRequest;
 import spring.project.service.UserService;
+import spring.project.util.JwtTokenUtils;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 @Controller
+@RequiredArgsConstructor
+@Log4j2
 public class UserController {
 
     private final UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("/user/login/form") //로그인 화면으로 이동
-    public String loginForm(){
+    @GetMapping("/user/login") //로그인 화면으로 이동
+    public String loginForm(Model model, @RequestParam(required = false, defaultValue = "") String referer){
+        if(!referer.isEmpty())
+            model.addAttribute("referer", referer);
         return "/user/login";
     }
 
-    @PostMapping("/user/login") //로그인
-    public String login(HttpSession session, LoginForm loginForm, Model model){
-        //로그인 기능 구현
-        User loginUser = new User();
-        loginUser.setId(loginForm.getId());
-        loginUser.setPassword(loginForm.getPassword());
+//    @PostMapping("/user/login") //로그인
+//    public String login(HttpSession session, LoginForm loginForm, Model model){
+//        //로그인 기능 구현
+//        User loginUser = new User();
+//        loginUser.setId(loginForm.getId());
+//        loginUser.setPassword(loginForm.getPassword());
+//
+//        try{
+//            loginUser = userService.login(loginUser);
+//        }catch (Exception e){
+//            model.addAttribute("exception", true);
+//            model.addAttribute("message", e.getMessage());
+//            return "/user/login";
+//        }
+//
+//        session.setAttribute("userId", loginUser.getId());
+//        session.setAttribute("userType", loginUser.getUserType());
+//        session.setMaxInactiveInterval(3600);
+//
+//        return "redirect:/";
+//    }
 
+    @PostMapping("/user/login") //로그인
+    public String login(HttpSession session, LoginRequest loginRequest, Model model, HttpServletResponse response,
+                        @RequestParam(required = false, defaultValue = "") String referer){
+
+
+        log.info("user login referer: {}", referer);
+        String token;
         try{
-            loginUser = userService.login(loginUser);
-        }catch (Exception e){
+            token = userService.login2(loginRequest);
+            JwtTokenUtils.addCookie("token", token, response);
+        } catch (Exception e){
             model.addAttribute("exception", true);
             model.addAttribute("message", e.getMessage());
-            return "/user/login";
+            return "/user/login?referer="+referer;
         }
 
-        session.setAttribute("userId", loginUser.getId());
-        session.setAttribute("userType", loginUser.getUserType());
+        session.setAttribute("userId", loginRequest.getId());
+        session.setAttribute("userType", loginRequest.getUserType());
         session.setMaxInactiveInterval(3600);
+
+        if(!referer.isEmpty()) {
+            return "redirect:"+ URLDecoder.decode(referer, StandardCharsets.UTF_8);
+        }
 
         return "redirect:/";
     }
 
     @GetMapping("/user/logout") //로그아웃 -> 메인페이지로
-    public String logout(HttpSession httpSession){
-        httpSession.invalidate();
+    public String logout(HttpSession httpSession, HttpServletResponse response){
+        httpSession.removeAttribute("userId");
+        httpSession.removeAttribute("userType");
+        JwtTokenUtils.removeCookie("token", response);
         return "redirect:/";
     }
 
-    @GetMapping("/user/create/form") //회원가입 화면으로 이동
+    @GetMapping("/user/create") //회원가입 화면으로 이동
     public String createForm(){
         return "/user/create";
     }
 
     @PostMapping("/user/create") //회원가입
-    public String create(CreateForm createForm, Model model){
+    public String create(@Valid UserJoinRequest userJoinRequest, BindingResult bindingResult, Model model){
 
         try{
-            userService.validateDuplicateUser(createForm.getId());
+//            userService.validateDuplicateUser(createForm.getId());
+            userService.join2(userJoinRequest);
         }catch (Exception e){
             model.addAttribute("exception", true);
             model.addAttribute("message", e.getMessage());
             return "/user/create";
         }
 
-        User user = new User();
-        user.setId(createForm.getId());
-        user.setEmail(createForm.getEmail());
-        user.setUserType(createForm.getUserType());
-        user.setPassword(createForm.getPassword());
-        user.setName(createForm.getName());
+//        User user = new User();
+//        user.setId(createForm.getId());
+//        user.setEmail(createForm.getEmail());
+//        user.setUserType(createForm.getUserType());
+//        user.setPassword(createForm.getPassword());
+//        user.setName(createForm.getName());
 
-        userService.join(user);
+//        userService.join(user);
 
         return "redirect:/";
     }
@@ -92,4 +132,6 @@ public class UserController {
         model.addAttribute("userlist" , userService.findUsers());
         return "/admin/userlist";
     }
+
+
 }
--- project/src/main/java/spring/project/service/UserService.java
+++ project/src/main/java/spring/project/service/UserService.java
@@ -1,27 +1,53 @@
 package spring.project.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring.project.domain.User;
-import spring.project.repository.MemoryUserRepository;
+import spring.project.dto.LoginRequest;
+import spring.project.dto.UserJoinRequest;
 import spring.project.repository.UserRepository;
+import spring.project.util.JwtTokenUtils;
 
 import java.util.List;
 import java.util.Optional;
 
-//@Service
-@Transactional
+@Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+    @Value("${jwt.expired-time-ms}")
+    private long expiredTimeMs;
 
     public String join(User user){
         validateDuplicateUser(user.getId());
         userRepository.save(user);
         return user.getId();
+    }
+
+    @Transactional
+    public User join2(UserJoinRequest userinfo) {
+        userRepository.findById(userinfo.getId()).ifPresent( result -> {
+            throw new IllegalStateException("이미 사용 중인 아이디 입니다.");
+                });
+
+        User joinUser = User.builder()
+                .id(userinfo.getId())
+                .userType(userinfo.getUserType())
+                .email(userinfo.getEmail())
+                .name(userinfo.getName())
+                .password(encoder.encode(userinfo.getPassword()))
+                .build();
+
+        userRepository.save(joinUser);
+
+        return joinUser;
     }
 
     public void validateDuplicateUser(String id){
@@ -52,5 +78,24 @@ public class UserService {
                 }
         );
         return result.get();
+    }
+
+    public String login2(LoginRequest loginRequest) {
+        Optional<User> result = userRepository.findById(loginRequest.getId());
+
+        if(result.isEmpty()){
+            throw new IllegalStateException(loginRequest.getId() + "는 존재하지 않는 아이디 입니다.");
+        }
+        result.ifPresent(
+                userinfo -> {
+                    if(!encoder.matches(loginRequest.getPassword(), userinfo.getPassword())) {
+                        throw new IllegalStateException("비밀번호가 일치하지 않습니다.");
+                    }
+                }
+        );
+
+
+        //토큰 생성
+        return JwtTokenUtils.generateToken(loginRequest.getId(), secretKey, expiredTimeMs);
     }
 }

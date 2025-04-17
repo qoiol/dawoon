af82fad291bc9fe0c42d80f784f54992a8ec7c9a
qoiol <hjjj1324@gmail.com>
2025-04-17 22:48:33 +0900

    dto 어노테이션 추가
    Signed-off-by: qoiol <hjjj1324@gmail.com>

--- project/src/main/java/spring/project/dto/ReviewForm.java
+++ project/src/main/java/spring/project/dto/ReviewForm.java
@@ -1,40 +1,28 @@
 package spring.project.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewForm {
+    @NotBlank(message = "제목을 입력해주세요.")
+    @Size(min=1, max=20, message = "제목을 20자 이내로 입력해주세요.")
     String title;
+    @NotBlank(message = "내용을 입력해주세요.")
+    @Size(min=1, max=300, message = "내용을 300자 이내로 입력해주세요.")
     String content;
+    @NotBlank(message = "후기를 남길 강좌를 선택해주세요.")
     long workoutId;
+    @Min(value = 1, message = "점수를 입력해주세요.")
+    @Max(value = 5, message = "정확한 점수를 입력해주세요.(1-5)")
     int score;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public long getWorkoutId() {
-        return workoutId;
-    }
-
-    public void setWorkoutId(long workoutId) {
-        this.workoutId = workoutId;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
 }

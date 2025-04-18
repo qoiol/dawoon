00d8757e87c2956d5eec9e3bbea337e5d748545a
qoiol <hjjj1324@gmail.com>
2025-04-18 16:52:01 +0900

    validation 추가
    Signed-off-by: qoiol <hjjj1324@gmail.com>

--- project/src/main/java/spring/project/dto/ReviewForm.java
+++ project/src/main/java/spring/project/dto/ReviewForm.java
@@ -14,15 +14,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReviewForm {
-    @NotBlank(message = "제목을 입력해주세요.")
+    @NotBlank(message = "제목을 20자 이내로 입력해주세요.")
     @Size(min=1, max=20, message = "제목을 20자 이내로 입력해주세요.")
     String title;
-    @NotBlank(message = "내용을 입력해주세요.")
+    @NotBlank(message = "내용을 300자 이내로 입력해주세요.")
     @Size(min=1, max=300, message = "내용을 300자 이내로 입력해주세요.")
     String content;
     @NotBlank(message = "후기를 남길 강좌를 선택해주세요.")
     long workoutId;
-    @Min(value = 1, message = "점수를 입력해주세요.")
-    @Max(value = 5, message = "정확한 점수를 입력해주세요.(1-5)")
+    @Min(value = 1, message = "점수를 입력해주세요.(1-5)")
+    @Max(value = 5, message = "점수를 입력해주세요.(1-5)")
     int score;
 }
